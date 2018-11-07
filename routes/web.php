6f13e181fa8d343a16e0bf17bf7e593be9f76aaa
Juliooo <juliogrupo935@gmail.com>
2018-11-06 19:41:58 -0800

    Nuevas cosas hyena,bad

--- routes/web.php
+++ routes/web.php
@@ -31,16 +31,13 @@ Route::get('VerChim', function () {
     return $tab1;
 });
 
+
 Route::get('VerWol', function () {
 
     $tab1 = DB::table('wolverines')->get();
 
     return $tab1;
 });
-
-
-
-
 
 Route::get('VerTodos', function () {
     $tab1 = DB::table('chimpanzes')->get();
@@ -54,9 +51,117 @@ Route::get('VerTodos', function () {
 });
 
 
+/// hyena ///
 
+Route::get('Verhyena', function () {
+    $tab1 = DB::table('hyenas')->get();
+    return $tab1;
+});
+
+Route::post('Hyena/add',function(Request $request){
+    $hyenas = new hyena ;
+    $hyenas->nombre = $request-> input('nombre');
+    $hyenas->genero = $request-> input('genero');
+    $hyenas->color = $request-> input('color');
+    $hyenas->edad = $request-> input('edad');
+    $hyenas->animal_id = $request-> input('animal_id');
+    $hyenas->save();
+    echo 'La ID ' .$hyenas;
+});
+
+
+Route::put('Hyena/update/{id}' , function (Request $request , $id){
+    $hyenas = hyena::find($id);
+    $hyenas->nombre = $request-> input('nombre');
+    $hyenas->genero = $request-> input('genero');
+    $hyenas->color = $request-> input('color');
+    $hyenas->edad = $request-> input('edad');
+    $hyenas->animal_id = $request-> input('animal_id');
+    $hyenas->save();
+    echo 'La ID ' .$hyenas;
+});
+
+Route::get('Hyena/delete/{id}', function ($id) {
+    hyena::destroy($id);
+    return 'hyenas'.$id.' deleted';
+});
+
+
+//// chimpanze /////
+
+Route::get('Verchimp', function () {
+    $tab1 = DB::table('chimpanzes')->get();
+    return $tab1;
+});
+
+Route::post('Chimp/add',function(Request $request){
+    $chimpanzes = new chimpanze ;
+    $chimpanzes->nombre = $request-> input('nombre');
+    $chimpanzes->genero = $request-> input('genero');
+    $chimpanzes->color = $request-> input('color');
+    $chimpanzes->edad = $request-> input('edad');
+    $chimpanzes->animal_id = $request-> input('animal_id');
+    $chimpanzes->save();
+    echo 'La ID ' .$chimpanzes;
+});
+
+
+Route::put('Chimp/update/{id}' , function (Request $request , $id){
+    $chimpanzes = chimpanze::find($id);
+    $chimpanzes->nombre = $request-> input('nombre');
+    $chimpanzes->genero = $request-> input('genero');
+    $chimpanzes->color = $request-> input('color');
+    $chimpanzes->edad = $request-> input('edad');
+    $chimpanzes->animal_id = $request-> input('animal_id');
+    $chimpanzes->save();
+    echo 'La ID ' .$chimpanzes;
+});
+
+Route::get('Chimp/delete/{id}', function ($id) {
+    chimpanze::destroy($id);
+    return 'chimpanze'.$id.' deleted';
+});
+
+
+ //// Budgeringar ////
+Route::get('VerBudg', function () {
+    $tab1 = DB::table('budgeringars')->get();
+    return $tab1;
+});
+
+Route::post('Budg/add',function(Request $request){
+    $budgeringars = new budgeringar ;
+    $budgeringars->nombre = $request-> input('nombre');
+    $budgeringars->genero = $request-> input('genero');
+    $budgeringars->color = $request-> input('color');
+    $budgeringars->edad = $request-> input('edad');
+    $budgeringars->animal_id = $request-> input('animal_id');
+    $budgeringars->save();
+    echo 'La ID ' .$budgeringars;
+});
+
+Route::put('Budg/update/{id}' , function (Request $request , $id){
+    $budgeringars = budgeringar::find($id);
+    $budgeringars->nombre = $request-> input('nombre');
+    $budgeringars->genero = $request-> input('genero');
+    $budgeringars->color = $request-> input('color');
+    $budgeringars->edad = $request-> input('edad');
+    $budgeringars->animal_id = $request-> input('animal_id');
+    $budgeringars->save();
+    echo 'La ID ' .$budgeringars;
+});
+
+Route::get('/Budg/delete/{id}', function ($id) {
+    budgeringar::destroy($id);
+    return 'Budgeringar'.$id.' deleted';
+});
 
 /////  Wolverines ///////
+
+Route::get('VerWol', function () {
+    $tab1 = DB::table('wolverines')->get();
+    return $tab1;
+});
 
 Route::put('wol/update/{id}' , function (Request $request , $id){
     $wolverines = wolverine::find($id);
@@ -70,6 +175,7 @@ Route::put('wol/update/{id}' , function (Request $request , $id){
     echo 'La ID ' .$wolverines;
 });
 
+
 Route::post('wol/add',function(Request $request){
     $wolverines = new wolverine ;
     $wolverines->nombre = $request-> input('nombre');
@@ -78,7 +184,6 @@ Route::post('wol/add',function(Request $request){
     $wolverines->edad = $request-> input('edad');
     $wolverines->animal_id = $request-> input('animal_id');
     $wolverines->save();
-
     echo 'La ID ' .$wolverines;
 });
 
@@ -87,6 +192,11 @@ Route::post('wol/add',function(Request $request){
 Route::get('/wol/delete/{id}', function ($id) {
     wolverine::destroy($id);
     return 'wolverine'.$id.' deleted';
+});
+
+Route::get('/wol/search/{id}', function ($id) {
+    wolverine::table($id);
+    return 'wolverine'.$id;
 });
 
 
